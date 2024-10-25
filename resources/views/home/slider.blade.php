936f00e30442588a4a91122ca9d34989dcfccdb8
Muhammad Asim <asim55374@gmail.com>
2024-10-25 14:30:15 +0500

    Updated slider view

--- resources/views/home/slider.blade.php
+++ resources/views/home/slider.blade.php
@@ -9,7 +9,7 @@
                                 <div class="detail-box">
                                     <h1>
                                         Welcome To Our <br>
-                                        Gift Shop
+                                        Asim
                                     </h1>
                                     <p>
                                         Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non
